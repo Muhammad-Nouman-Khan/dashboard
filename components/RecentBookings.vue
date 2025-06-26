bd56b835d92cfe9da32bceeb2c674f842ec21690
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-26 15:12:41 +0500

    fix: adjust table header classes for RecentBookings component for consistency

--- components/RecentBookings.vue
+++ components/RecentBookings.vue
@@ -10,10 +10,8 @@
           <th :class="headerCellClass + ' hidden md:table-cell'">Debit</th>
           <th :class="headerCellClass + ' hidden md:table-cell'">Credit</th>
           <th :class="headerCellClass">Total</th>
-          <th :class="'rounded-tr-lg '">Status</th>
-          <th :class="headerCellClass + ' rounded-tr-lg hidden sm:table-cell'">
-            Action
-          </th>
+          <th :class="headerCellClass">Status</th>
+          <th :class="' rounded-tr-lg hidden sm:table-cell'">Action</th>
         </tr>
       </thead>
       <tbody>
